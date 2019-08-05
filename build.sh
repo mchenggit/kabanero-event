f9d4427f326ebad40f795a648ef9b6edaa5422cd
Michael Cheng <mcheng@us.ibm.com>
2019-08-05 11:48:53 -0500

    update build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
-set -e pipefail
-if [ $(arch) = "ppc64le" ]; then
-  ARCH=ppc64le
-elif [ $(arch) = "s390x" ]; then
-  ARCH=s390x
-else
-  ARCH=amd64
-fi
 
-IMAGE=prism-was-controller
-registry=hyc-icpcontent-docker-local.artifactory.swg-devops.com
-
-echo "$ARTIFACTORY_DOCKER_PWD" | docker login $registry -u "$ARTIFACTORY_DOCKER_USER" --password-stdin
-
-docker pull ${registry}/prism-jre:8-jre-ubi-min-${ARCH}
-docker tag ${registry}/prism-jre:8-jre-ubi-min-${ARCH} ibmjava:8-jre-ubi-min
+IMAGE=kabanero-event
 
 echo "Building ${IMAGE}"
 
