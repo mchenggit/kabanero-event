f30347a91d14da57d6ec0347d75efb745562d0a4
Michael Cheng <mcheng@us.ibm.com>
2019-08-06 08:52:41 -0500

    more skeleton code

--- src/main/java/io/kabanero/event/Main.java
+++ src/main/java/io/kabanero/event/Main.java
@@ -1,4 +1,8 @@
 package io.kabanero.event;
+import io.kubernetes.client.ApiClient;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,6 +13,24 @@ public class Main {
     public static void main(String args[] ) {
         try {
             logger.info("Starting io.kabanero.event");
+
+
+            ApiClient apiClient = KubeUtils.getApiClient();
+
+            // disable host name  verification for HTTSURLConnection calls
+            HostnameVerifier trustAllHosts = new  HostnameVerifier() {
+                public boolean verify(String host, SSLSession session) { return true; }
+            };
+            HttpsURLConnection.setDefaultHostnameVerifier(trustAllHosts);
+
+           // TODO: Remove 
+           TektonUtils.test(apiClient);
+
+           for (; ; ) {
+               // TODO: remove after we put in an infinite loop to retrieve and process messages
+               Thread.currentThread().sleep(60000);
+           }
+
         }  catch(Throwable t) {
             logger.error("Error in WASController", t);
         }
---
+++ src/main/java/io/kabanero/event/TektonUtils.java
@@ -0,0 +1,20 @@
+package io.kabanero.event;
+import io.kubernetes.client.ApiClient;
+import io.kubernetes.client.ApiException;
+import io.kubernetes.client.Configuration;
+import io.kubernetes.client.apis.CoreV1Api;
+import io.kubernetes.client.apis.CustomObjectsApi;
+import io.kubernetes.client.util.Config;
+import io.kubernetes.client.models.V1DeleteOptions;
+import io.kubernetes.client.models.V1Secret;
+
+public class TektonUtils {
+
+    public static void test(ApiClient apiClient) {
+        // crate resources
+
+        // Delete existing run
+
+        // Create a new run
+    }
+}
